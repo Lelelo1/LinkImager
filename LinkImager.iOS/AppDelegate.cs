96606ba171eae2a2d0513d863f93ee70c1a5b5fd
hemma <Leo.W.se@hotmail.com>
2018-12-19 12:44:05 +0100

    Removed test UiTestProject. added calendar permission to conform with app store connect mail

--- LinkImager.iOS/AppDelegate.cs
+++ LinkImager.iOS/AppDelegate.cs
@@ -23,7 +23,6 @@ namespace LinkImager.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Xamarin.Calabash.Start();
             global::Xamarin.Forms.Forms.Init();
             MR.Gestures.iOS.Settings.LicenseKey = "QY37-QU4R-YH9Q-C9NA-6MTS-J5CV-3WE7-MVA4-7FDE-ZF8K-U8GP-FK5R-W6XN";
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
